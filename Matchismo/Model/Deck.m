58a954cf66918298be74671b05df2350733e1990
kokoloco64 <kokoloco.studios@gmail.com>
2013-01-26 14:49:39 -0500

    Added PlayingCard and PlayingCardDeck Model Classes from Lecture 2.

--- Matchismo/Model/Deck.m
+++ Matchismo/Model/Deck.m
@@ -8,6 +8,41 @@
 
 #import "Deck.h"
 
+@interface Deck()
+@property (strong, nonatomic) NSMutableArray *cards; // of cards
+@end
+
 @implementation Deck
+
+- (NSMutableArray *)cards
+{
+    if (!_cards) { // Lazy Instantiation
+        _cards = [[NSMutableArray alloc] init];
+    }
+    return _cards;
+}
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop
+{
+    if (card) {
+        if (atTop) {
+            [self.cards insertObject:card atIndex:0];
+        } else {
+            [self.cards addObject:card];
+        }
+    }
+}
+
+- (Card *)drawRandomCard
+{
+    Card *randomCard;
+    
+    if (self.cards.count) {
+        unsigned index = arc4random() % self.cards.count;
+        randomCard = self.cards[index];
+        [self.cards removeObjectAtIndex:index];
+    }
+    return randomCard;
+}
 
 @end
--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -9,5 +9,46 @@
 #import "PlayingCard.h"
 
 @implementation PlayingCard
+@synthesize suit = _suit; // because we provided both getter & setter
+
++ (NSArray *)validSuits
+{
+    return @[@"♠", @"♣", @"♥", @"♦"];
+}
+
+- (NSString *)contents
+{
+    NSArray *rankStrings = [PlayingCard rankStrings];
+    return [rankStrings[self.rank] stringByAppendingString:self.suit];
+}
+
+- (void)setSuit:(NSString *)suit
+{
+    if ([[PlayingCard validSuits] containsObject:suit]) {
+        _suit = suit;
+    }
+}
+
+- (NSString *)suit
+{
+    return _suit ? _suit : @"?";
+}
+
++ (NSArray *)rankStrings
+{
+    return @[@"?", @"A", @"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J",@"Q",@"K"];
+}
+
++ (NSUInteger)maxRank
+{
+    return [self rankStrings].count-1;
+}
+
+- (void)setRank:(NSUInteger)rank
+{
+    if (rank <= [PlayingCard maxRank]) {
+        _rank = rank;
+    }
+}
 
 @end
