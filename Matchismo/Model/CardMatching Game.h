80bc84ea27297b6d52a84bceb751d21224b8d94e
kokoloco64 <kokoloco.studios@gmail.com>
2013-02-03 18:26:06 -0500

    First HW #1 To-Do: upgraded to Lecture 3.  Multi-cards + CardMatchingGame game model.

---
+++ Matchismo/Model/CardMatching Game.h
@@ -0,0 +1,13 @@
+//
+//  CardMatching Game.h
+//  Matchismo
+//
+//  Created by Dulio Denis on 2/3/13.
+//  Copyright (c) 2013 Stanford University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatching_Game : NSObject
+
+@end
---
+++ Matchismo/Model/CardMatching Game.m
@@ -0,0 +1,13 @@
+//
+//  CardMatching Game.m
+//  Matchismo
+//
+//  Created by Dulio Denis on 2/3/13.
+//  Copyright (c) 2013 Stanford University. All rights reserved.
+//
+
+#import "CardMatching Game.h"
+
+@implementation CardMatching_Game
+
+@end
